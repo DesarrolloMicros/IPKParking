0dce4ddf9da8014e053ce002e74d7210fb26eefd
gp00 <jlgp1970@gmail.com>
2018-11-09 13:35:06 +0100

    IOS Error 13:35

--- IpkParking/ios/IpkParking/AppDelegate.m
+++ IpkParking/ios/IpkParking/AppDelegate.m
@@ -21,7 +21,8 @@
 
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
 
-  [FIRApp configure];
+  /**[FIRApp configure];**/
+  FirebaseApp.configure();
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"IpkParking"
