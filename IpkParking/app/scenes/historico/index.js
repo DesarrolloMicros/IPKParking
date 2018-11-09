fd8dfedac246663a6b167a08c93bf141c8f88c76
gp00 <jlgp1970@gmail.com>
2018-11-09 11:55:29 +0100

    IOS Error 11:55

--- IpkParking/app/scenes/historico/index.js
+++ IpkParking/app/scenes/historico/index.js
@@ -17,7 +17,7 @@ import { Actions } from 'react-native-router-flux';
 
 import Cabecera from '../../components/cabecera';
 import Pie from '../../components/pie'
-import AñosMeses from '../../components/añosMeses'
+import AñosMeses from './ams'
 import MovimientosHistorico from '../../components/movimientosHistorico'
 
 import * as literal from '../../constants/literales';
