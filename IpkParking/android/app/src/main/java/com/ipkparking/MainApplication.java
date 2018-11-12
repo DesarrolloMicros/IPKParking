16df52dd08d91a33482d41b1af74ec85aaa6558b
gp00 <jlgp1970@gmail.com>
2018-11-12 11:15:16 +0100

    Error IOS 11:15

--- IpkParking/android/app/src/main/java/com/ipkparking/MainApplication.java
+++ IpkParking/android/app/src/main/java/com/ipkparking/MainApplication.java
@@ -3,6 +3,7 @@ package com.ipkparking;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.invertase.firebase.RNFirebasePackage;
 import com.evollu.react.fcm.FIRMessagingPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFirebasePackage(),
             new FIRMessagingPackage(),
             new VectorIconsPackage()
       );
