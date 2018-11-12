31f5c57085b2b4b25dab9d3783bd34c101eff98a
gp00 <jlgp1970@gmail.com>
2018-11-12 13:34:36 +0100

    Error IOS 13:35

--- IpkParking/ios/IpkParking/AppDelegate.m
+++ IpkParking/ios/IpkParking/AppDelegate.m
@@ -1,11 +1,15 @@
 /**
  * Copyright (c) 2015-present, Facebook, Inc.
+ * All rights reserved.
  *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
  */
 
 #import "AppDelegate.h"
+
+#import <Firebase.h>
 
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
@@ -17,6 +21,9 @@
   NSURL *jsCodeLocation;
 
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+
+  [FIRApp configure];
+ /** FirebaseApp.configure();**/
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"IpkParking"
@@ -33,3 +40,4 @@
 }
 
 @end
+
